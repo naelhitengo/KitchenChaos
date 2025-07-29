16721be27eb47bb9d2290462e75ad7f025f6d00a
Nael HITENGO <naelhitengo@gmail.com>
2025-07-30 01:21:27 +0200

    end of Unity Layout and Visual Studio

---
+++ Assets/Testing.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class Testing : MonoBehaviour
+{
+    private void Start() {
+        if (transform == transform)
+        {
+            Update();
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    // DO something
+                }
+            }
+        }
+    }
+
+    private void Update() {
+
+    }
+
+}
