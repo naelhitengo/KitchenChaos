ef9f9f01a2cf3c964a5c8ec36a5cf2066dab6ec3
Nael HITENGO <naelhitengo@gmail.com>
2025-09-01 15:16:17 +0200

    container counter 3 (end)

--- Assets/Scripts/ClearCounter.cs
+++ Assets/Scripts/ClearCounter.cs
@@ -3,47 +3,9 @@ using UnityEngine;
 public class ClearCounter : BaseCounter, IKitchenObjectParent
 {
     [SerializeField] private KitchenObjectSO kitchenObjectSO;
-    [SerializeField] private Transform counterTopPoint;
-
-    private KitchenObject kitchenObject;
 
     public override void Interact(Player player)
     {
-        if (kitchenObject == null)
-        {
-            Transform kitchenObjectTransform = Instantiate(kitchenObjectSO.prefab, counterTopPoint);
-            kitchenObjectTransform.GetComponent<KitchenObject>().SetKitchenObjectParent(this);
-        }
-        else
-        {
-            // Give the object to the player
-            kitchenObject.SetKitchenObjectParent(player);
-        }
-
     }
 
-    public Transform GetKitchenObjectFollowTransform()
-    {
-        return counterTopPoint;
-    }
-
-
-    public void SetKitchenObject(KitchenObject kitchenObject)
-    {
-        this.kitchenObject = kitchenObject;
-    }
-
-    public KitchenObject GetKitchenObject()
-    {
-        return kitchenObject;
-    }
-    public void ClearKitchenObject()
-    {
-        kitchenObject = null;
-    }
-
-    public bool HasKitchenObject()
-    {
-        return kitchenObject != null;
-    }
 }
---
+++ Assets/Scripts/ContainerCounterVisual.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class ContainerCounterVisual : MonoBehaviour
+{
+
+    private const string OPEN_CLOSE = "OpenClose";
+
+    [SerializeField] private ContainerCounter containerCounter;
+
+    private Animator animator;
+
+    public void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    private void Start()
+    {
+
+        containerCounter.OnPlayerGrabbedObject += ContainerCounter_OnPpayerGrabbedObject;
+
+    }
+
+    private void ContainerCounter_OnPpayerGrabbedObject(object sender, System.EventArgs e)
+    {
+        animator.SetTrigger(OPEN_CLOSE);
+    }
+}
