1228b4d6b8ac4b27dc813e53817a1ac4548c5052
naelhitengo <naelhitengo@gmail.com>
2025-09-25 00:51:22 +0200

    Delivery Manager 1, verifier tout ok

--- Assets/Scripts/Counters/DeliveryCounter.cs
+++ Assets/Scripts/Counters/DeliveryCounter.cs
@@ -9,6 +9,9 @@ public class DeliveryCounter : BaseCounter
             if (player.GetKitchenObject().TryGetPlate(out PlateKitchenObject plateKitchenObject))
             {
                 // Only accepts Plates
+
+                DeliveryManager.Instance.DeliverRecipe(plateKitchenObject);
+
                 player.GetKitchenObject().DestroySelf();
             }
         }
---
+++ Assets/Scripts/ScriptableObjects/RecipeListSO.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//[CreateAssetMenu()]
+// Dans le tuto, on a besoin que d'une liste donc on commente le createAssetMenu.
+// Aussi comme il n'y en a qu'une , on le laise dans le dossier des RecipeSO
+// et prefixé de l'underscore pour le faire apparaitre en haut de la liste.
+public class RecipeListSO : ScriptableObject
+{
+    public List<RecipeSO> recipeSOList;
+
+}
